badad77040a9fe703eec6403ca1e01198c6a5802
Javae Viittoni <javaevittoni@gmail.com>
2025-02-08 22:15:51 -0800

    make floating action button to add assignments active // SECTION 1 COMPLETE

--- app/src/main/java/com/example/studentplanner03/UI/CourseDetails.java
+++ app/src/main/java/com/example/studentplanner03/UI/CourseDetails.java
@@ -1,6 +1,8 @@
 package com.example.studentplanner03.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.studentplanner03.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CourseDetails extends AppCompatActivity {
 
@@ -21,6 +24,15 @@ public class CourseDetails extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CourseDetails.this, AssignmentDetails.class);
+                startActivity(intent);
+            }
         });
     }
 }
