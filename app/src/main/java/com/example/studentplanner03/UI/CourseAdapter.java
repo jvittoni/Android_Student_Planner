5f3d448612833d9b7b43c51c9e971f38ecf626be
Javae Viittoni <javaevittoni@gmail.com>
2025-02-10 12:02:50 -0800

    created CourseAdapter

---
+++ app/src/main/java/com/example/studentplanner03/UI/CourseAdapter.java
@@ -0,0 +1,87 @@
+package com.example.studentplanner03.UI;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.studentplanner03.R;
+import com.example.studentplanner03.entities.Course;
+
+import java.util.List;
+
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseViewHolder> {
+
+    private List<Course> mCourses;
+    private final Context context;
+    private final LayoutInflater mInflator;
+
+    public CourseAdapter(Context context) {
+        mInflator = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    public class CourseViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView courseItemView;
+
+        public CourseViewHolder(@NonNull View itemView) {
+            super(itemView);
+            courseItemView = itemView.findViewById(R.id.textView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    final Course current = mCourses.get(position);
+                    Intent intent = new Intent(context, CourseDetails.class);
+                    intent.putExtra("crseId", current.getCourseID());
+                    intent.putExtra("crseName", current.getCourseName());
+                    intent.putExtra("crseInstruct", current.getCourseInstructor());
+                    intent.putExtra("crseStartDate", current.getCourseStartDate());
+                    intent.putExtra("crseEndDate", current.getCourseEndDate());
+                    intent.putExtra("crseDesc", current.getCourseDescription());
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+
+    @NonNull
+    @Override
+    public CourseAdapter.CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflator.inflate(R.layout.course_list_item, parent, false);
+        return new CourseViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseAdapter.CourseViewHolder holder, int position) {
+        if (mCourses != null) {
+            Course current = mCourses.get(position);
+            String crseName = current.getCourseName();
+            holder.courseItemView.setText(crseName);
+        }
+        else {
+            holder.courseItemView.setText("No class name");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mCourses != null) {
+            return mCourses.size();
+        }
+        else return 0;
+    }
+
+    public void setCourses(List<Course> courses) {
+        mCourses = courses;
+        notifyDataSetChanged();
+    }
+
+
+}
